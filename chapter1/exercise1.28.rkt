35973774e954289b2a66a45b1a4b9515e9925253
headcubicle <speed-restriction@iijmio-mail.jp>
2014-12-31 12:04:21 +0900

    exercise1.28.rktを追加

---
+++ chapter1/exercise1.28.rkt
@@ -0,0 +1,22 @@
+#lang planet neil/sicp
+
+(define (square n) (* n n))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((and (not (or (= base 1) (= base (- m 1)))) (= (remainder (square base) m) 1)) 0)
+        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (miller-rabin-test n)
+  (try-it (+ 1 (random (- n 1))) n))
+
+(define (try-it a n) (= (expmod a (- n 1) n) 1))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((miller-rabin-test n) (fast-prime? n (- times 1)))
+        (else false)))
