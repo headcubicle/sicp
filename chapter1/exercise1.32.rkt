616e8796ec24281d6f8c1dd63332d99af0f4633c
headcubicle <speed-restriction@iijmio-mail.jp>
2015-05-21 14:43:32 +0900

    exercise1.32.rktを追加

---
+++ chapter1/exercise1.32.rkt
@@ -0,0 +1,29 @@
+#lang planet neil/sicp
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+;(define (accumulate combiner null-value term a next b)
+;  (if (> a b)
+;      null-value
+;      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+         
+(define (identity x) x)
+
+(define (inc n) (+ n 1))
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(define (sum-integers a b)
+  (sum identity a inc b))
