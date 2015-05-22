28207ade117f99e76f01102e7ac9e0d272d8c819
headcubicle <speed-restriction@iijmio-mail.jp>
2015-05-22 13:44:28 +0900

    1.3.1のサンプルコードとexercise1.35.rktを追加

---
+++ chapter1/fixed-point.rkt
@@ -0,0 +1,23 @@
+#lang planet neil/sicp
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
