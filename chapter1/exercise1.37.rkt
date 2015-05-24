fa88f1de6ea1ac761e2349ef0ff12af86c8839a2
headcubicle <speed-restriction@iijmio-mail.jp>
2015-05-24 16:51:25 +0900

    exercise1.37.rktからexercise1.39.rktを修正

--- chapter1/exercise1.37.rkt
+++ chapter1/exercise1.37.rkt
@@ -4,11 +4,11 @@
 (define (cont-frac-i n d k)
   (define (cont-frac-iter x i)
     (define (fraction x)
-      (+ (d i) (/ (n i) x)))
+      (/ (n i) (+ (d i) x)))
     (let ((next (fraction x)))
       (if (> i 1)
           (cont-frac-iter next (- i 1))
-          (/ (n i) next))))
+          next)))
   (cont-frac-iter (d k) k))
 
 (define (cont-frac-a x)
@@ -29,4 +29,5 @@
 (define (cont-frac-b x)
   (cont-frac-r (lambda (i) 1.0)
                (lambda (i) 1.0)
-               x))
+               x
+               1))
--- chapter1/exercise1.38.rkt
+++ chapter1/exercise1.38.rkt
@@ -4,11 +4,11 @@
 (define (cont-frac-i n d k)
   (define (cont-frac-iter x i)
     (define (fraction x)
-      (+ (d i) (/ (n i) x)))
+      (/ (n i) (+ (d i) x)))
     (let ((next (fraction x)))
       (if (> i 1)
           (cont-frac-iter next (- i 1))
-          (/ (n i) next))))
+          next)))
   (cont-frac-iter (d k) k))
 
 (define (cont-frac-a x)
