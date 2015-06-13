22e4adc5c457a90bb57b81ee7220daf9554cb036
headcubicle <speed-restriction@iijmio-mail.jp>
2015-06-13 17:54:43 +0900

    rat.rktとexercise2.1.rktを追加

---
+++ chapter2/exercise2.1.rkt
@@ -0,0 +1,35 @@
+#lang planet neil/sicp
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (make-rat n d)
+  (let ((absn (abs n))
+        (absd (abs d))
+        (g (gcd (abs n) (abs d))))
+    (if (> (* n d) 0) (cons (/ absn g) (/ absd g))
+        (cons (/ (* -1 absn) g) (/ absd g)))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
